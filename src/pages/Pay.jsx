718f7b861d519f29fc0028d7514633a830e00270
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2023-03-06 17:51:36 +0300

    complete

--- src/pages/Pay.jsx
+++ src/pages/Pay.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import EthereumQRPlugin from 'ethereum-qr-code'
 import QRCode from 'qrcode.react'
 import { useParams } from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
+import { ethers } from 'ethers'
 
 const Pay = () => {
   const { session } = useParams()
   let decodedData = JSON.parse(decodeURIComponent(session))
 
+  const [qrData, setQrData] = useState()
+  const qr = new EthereumQRPlugin()
+
+  useEffect(() => {
+    generateQRCode()
+  }, [decodedData])
+
+  const generateQRCode = async () => {
+    const qrData = qr.toDataUrl({
+      to: decodedData.address,
+      value: ethers.parseEther(decodedData.amount),
+      chainId: 44787,
+    })
+
+    qrData.then((code) => {
+      console.log('Your QR id generated:', code.value)
+      setQrData(code.value)
+    })
+  }
+
   return (
     <div className="flex p-10 flex-1 flex-col items-center justify-center">
       <div className="w-96 h-80 rounded-md flex items-center justify-center bg-gray-50">
-        <QRCode className="w-full h-full" value={session} />
+        {qrData && <QRCode className="w-full h-full" value={qrData} />}
       </div>
-      <p>User ID: {decodedData.address}</p>
-      <p>User Name: {decodedData.amount}</p>
     </div>
   )
 }
